04a175db64fb49c09866011c1cb60d3071192830
Fajar Hidayah <fajarramadhanhidayah@gmail.com>
2018-01-18 13:21:03 +0700

    create header component and add to home page

---
+++ vue_training/src/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="header">
+    <ul>
+      <li>{{menu}}</li>
+    </ul>
+  </div>
+</template>
+<script type="text/javascript">
+  export default{
+    name: "header",
+    data() {
+      return{
+        menu: "menu"
+      }
+    }
+  }
+</script>
+<style media="screen">
+
+</style>
